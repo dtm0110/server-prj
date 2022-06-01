ebf3e47e20d45de1d4b8e55bcdec2a7ce604f106
dtm0110 <tuanmanhykkt@gmail.com>
2022-06-01 17:25:58 +0700

    v2

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const fs = require('fs')
+const cors=require("cors");
 const bodyParser = require('body-parser')
 const { json } = require('body-parser')
 
 const app = express()
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
+app.use(cors(corsOptions))
 const PORT = 9000
 app.listen(PORT, () => console.log('listening on port ' + PORT))
 
@@ -12,11 +19,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-const dog = {
-    name: 'Dog',
-    age: 36777
-}
 
 const saveData = (dog) => {
 
@@ -33,10 +35,36 @@ const saveData = (dog) => {
     }
 
     const jsonData = JSON.stringify(dog)
-    fs.writeFile('C:/Users/Admin/Desktop/photo-app/src/assets/data/test.json', jsonData, finished)
+    fs.writeFile('C:/Users/Administrator/Desktop/site-user/src/assets/test.json', jsonData, finished)
 }
  
+
+const saveOrder = (dog) => {
+
+    console.log("abcxyz")
+    console.log("data truyen vao order" + dog)
+    const data = {
+        product: dog
+    }
+
+    const finished = (err) => {
+        if(err) {
+            console.error(err)
+            return
+        }
+    }
+
+    const jsonData = JSON.stringify(dog)
+    fs.writeFile('C:/Users/Administrator/Desktop/site-user/src/assets/ordertest.json', jsonData, finished)
+}
 //saveData(dog)
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 
 app.post("/data", (req, res) => {
     //res.status(201).send(req.body)
@@ -46,3 +74,14 @@ app.post("/data", (req, res) => {
     saveData(JSON.parse(result)); 
 })
 
+app.post("/order", (req, res) => {
+    //res.status(201).send(req.body)
+    console.log("da vao day")
+    console.log("order " + req.body)
+    //console.log("body gui len " + Object.keys(req.body))
+    console.log("in lan cuoi")
+    let result =Object.keys(req.body)[0] + "[" + Object.keys(Object.values(req.body)[0]) +"," + Object.keys(Object.values(Object.values(req.body)[0]))  ;
+    result = result.slice(0,-2) + "]}]"
+    //console.log(Object.keys(req.body)[0])
+    saveOrder(req.body); 
+})

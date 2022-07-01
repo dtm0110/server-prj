b553770c6216b54373796b65ae451a4620505a7c
dtm0110 <tuanmanhykkt@gmail.com>
2022-07-01 14:43:41 +0700

    last

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const cors=require("cors");
 const bodyParser = require('body-parser')
 const { json } = require('body-parser')
 
-const urlProduct = "C:/Users/Administrator/Desktop/site-user/src/assets/test.json"
-const urlOrder = "C:/Users/Administrator/Desktop/site-user/src/assets/ordertest.json"
-const urlUser = "C:/Users/Administrator/Desktop/site-user/src/assets/userTest.json"
+// const urlProduct = "C:/Users/Administrator/Desktop/site-user/src/assets/test.json"
+// const urlOrder = "C:/Users/Administrator/Desktop/site-user/src/assets/ordertest.json"
+// const urlUser = "C:/Users/Administrator/Desktop/site-user/src/assets/userTest.json"
+
+
+const urlProduct = "../site-user/src/assets/test.json"
+const urlOrder = "../site-user/src/assets/ordertest.json"
+const urlUser = "../site-user/src/assets/userTest.json"
+
+
 
 const app = express()
 const corsOptions ={
@@ -30,45 +37,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-const saveData = (dog) => {
-
-    console.log("data truyen vao" + dog)
-    const data = {
-        product: dog
-    }
-
-    const finished = (err) => {
-        if(err) {
-            console.error(err)
-            return
-        }
-    }
-
-    const jsonData = JSON.stringify(dog)
-    fs.writeFile('C:/Users/Administrator/Desktop/site-user/src/assets/test.json', jsonData, finished)
-}
- 
-
-const saveOrder = (dog) => {
-
-    console.log("abcxyz")
-    console.log("data truyen vao order" + dog)
-    const data = {
-        product: dog
-    }
-
-    const finished = (err) => {
-        if(err) {
-            console.error(err)
-            return
-        }
-    }
-
-    const jsonData = JSON.stringify(dog)
-    fs.writeFile('C:/Users/Administrator/Desktop/site-user/src/assets/ordertest.json', jsonData, finished)
-}
-
 const saveDataApi = (dataRequest, url) => {
     const finished = (err) => {
         if(err) {
@@ -79,33 +47,22 @@ const saveDataApi = (dataRequest, url) => {
     const jsonData = JSON.stringify(dataRequest)
     fs.writeFile(url, jsonData, finished)
 }
-//saveData(dog)
-
 
 
 app.post("/data", (req, res) => {
-    //res.status(201).send(req.body)
-    //console.log(req.body)
     let result = "[" +Object.keys(req.body)[0] + "]";
-    //console.log(Object.keys(req.body)[0])
-    saveData(JSON.parse(result)); 
+    //saveData(JSON.parse(result)); 
+    saveDataApi(JSON.parse(result), urlProduct); 
 })
 
 app.post("/order", (req, res) => {
-    //res.status(201).send(req.body)
-    console.log("da vao day")
-    console.log("order " + req.body)
-    //console.log("body gui len " + Object.keys(req.body))
-    console.log("in lan cuoi")
     let result =Object.keys(req.body)[0] + "[" + Object.keys(Object.values(req.body)[0]) +"," + Object.keys(Object.values(Object.values(req.body)[0]))  ;
     result = result.slice(0,-2) + "]}]"
-    //console.log(Object.keys(req.body)[0])
-    saveOrder(req.body); 
+    //saveOrder(req.body); 
+    saveDataApi(req.body, urlOrder); 
+
 })
 
 app.post("/user", (req,res) => {
-    console.log("da vao day")
-    console.log("order " + req.body)
-  
     saveDataApi(req.body, urlUser); 
 })
